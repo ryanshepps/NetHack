1683d45d6262101b642094f9e3ab1618d9ff737f
Ryan Sheppard <sheppardryan@rogers.com>
2020-10-02 11:34:11 -0400

    Basic Player class

--- src/main/java/rogue/Player.java
+++ src/main/java/rogue/Player.java
@@ -5,48 +5,47 @@ import java.awt.Point;
  * The player character
  */
 public class Player {
+      private String name;
+      private Point xyLocation;
+      private Room currentRoom;
 
 
+      // Default constructor
+      public Player() {
 
-    // Default constructor
-    public Player() {
-
-    }
-
-
-    public Player(String name) {
-
-    }
+      }
 
 
-    public String getName() {
-
-        return null;
-    }
-
-
-    public void setName(String newName) {
-
-    }
-
-    public Point getXyLocation() {
-        return null;
-
-    }
+      public Player(String name) {
+            this.name = name;
+      }
 
 
-    public void setXyLocation(Point newXyLocation) {
-
-    }
-
-
-    public Room getCurrentRoom() {
-        return null;
-
-    }
+      public String getName() {
+            return name;
+      }
 
 
-    public void setCurrentRoom(Room newRoom) {
+      public void setName(String newName) {
+            this.name = newName;
+      }
 
-    }
+      public Point getXyLocation() {
+            return xyLocation;
+      }
+
+
+      public void setXyLocation(Point newXyLocation) {
+            this.xyLocation = newXyLocation;
+      }
+
+
+      public Room getCurrentRoom() {
+            return currentRoom;
+      }
+
+
+      public void setCurrentRoom(Room newRoom) {
+            currentRoom = newRoom;
+      }
 }
